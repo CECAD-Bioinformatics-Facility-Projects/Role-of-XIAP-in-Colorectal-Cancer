8b20e700722af62c4e9e6642dbe871386b7a8b29
Ulrike Göbel <ugoebel@uni-koeln.de>
2026-08-27 13:44:54 +0000

    set an explicit list of requested terms

---
+++ scripts/requested_terms.R
@@ -0,0 +1,11 @@
+requested_terms <-
+  c(
+    "response to molecule of bacterial origin",
+    "humoral immune response",
+    "antimicrobial humoral response",
+    "response to lipopolysaccharide",
+    "antimicrobial humoral immune response mediated by antimicrobial peptide",
+    "cellular response to biotic stimulus",
+    "innate immune response in mucosa",
+    "cell killing"
+  )
